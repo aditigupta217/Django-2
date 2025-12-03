3d6112b62569093d01e86d9d6c26bf3aedf53846
aditigupta217 <aditigupta221177@gmail.com>
2025-12-03 11:44:41 +0530

    Initial commit

---
+++ employee/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    photo = models.ImageField(upload_to='images')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
