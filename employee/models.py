4fc7975c1357ae0a29b5dd4cac50a074ce08e8d0
aditigupta217 <aditigupta221177@gmail.com>
2025-12-03 23:05:59 +0530

    add

--- employee/models.py
+++ employee/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
-# Create your models here.
-
 class Employee(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    photo = models.ImageField(upload_to='images')
+    email = models.EmailField(max_length=100, unique=True)   
+    photo = models.ImageField(upload_to='images/')
+    phone_no = models.CharField(max_length=12, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
